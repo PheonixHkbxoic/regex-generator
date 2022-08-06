121033797115153ea00f0e76e900f4efe4dd5029
PheonixHkbxoic <hkbxoic@gmail.com>
2022-08-07 01:36:12 +0800

    feat: compressOneChar in Branches

--- src/main/java/cn/pheker/regex/generator/core/parser/nodes/Branches.java
+++ src/main/java/cn/pheker/regex/generator/core/parser/nodes/Branches.java
@@ -46,12 +46,40 @@ public class Branches extends AbstractComposite {
             }
             if (reCanBeEmpty.length() > 1) {
                 reCanBeEmpty = "(?:" + reCanBeEmpty + ")?";
-            }else{
+            } else {
                 reCanBeEmpty += "?";
             }
             return Collections.singletonList(reCanBeEmpty);
         }
-        String group = regex.stream().collect(Collectors.joining("|", "(?:", ")"));
+    
+        // one char, multi char
+        List<String> multi = compressOneChar(regex);
+        if (multi.size() == 1) {
+            return multi;
+        }
+        String group = multi.stream().collect(Collectors.joining("|", "(?:", ")"));
         return Collections.singletonList(group);
+    }
+    
+    private List<String> compressOneChar(List<String> regex) {
+        List<String> one = regex.stream()
+                .filter(r -> r.length() == 1)
+                .collect(Collectors.toList());
+        List<String> multi = new ArrayList<>();
+        if (one.size() != regex.size()) {
+            multi = regex.stream()
+                    .filter(r -> r.length() > 1)
+                    .collect(Collectors.toList());
+        }
+        if (!one.isEmpty()) {
+            String oneCompressed;
+            if (one.size() == 1) {
+                oneCompressed = one.get(0);
+            } else {
+                oneCompressed = one.stream().collect(Collectors.joining("", "[", "]"));
+            }
+            multi.add(0, oneCompressed);
+        }
+        return multi;
     }
 }
